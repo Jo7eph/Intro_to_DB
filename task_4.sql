3da0d2dfd1157089a39edc3f10785c5a127d7168
Youssef El Abdaoui <ssef.yu07@gmail.com>
2025-12-14 18:37:09 +0100

    Add task_4.sql: full description of books table using information_schema

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+-- Script to print the full description of the table 'books'
+-- Database name will be passed as argument when running the MySQL command
+-- All SQL keywords are in uppercase
+-- Example usage:
+-- mysql -u root -p alx_book_store < task_4.sql
+
+USE alx_book_store;
+
+SELECT COLUMN_NAME AS 'Column',
+       COLUMN_TYPE AS 'Type',
+       IS_NULLABLE AS 'Null',
+       COLUMN_KEY AS 'Key',
+       COLUMN_DEFAULT AS 'Default',
+       EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+  AND TABLE_NAME = 'books';
