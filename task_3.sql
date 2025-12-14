06c1abca8c4049cf198d9f5e8ad3864f53b27d6c
Youssef El Abdaoui <ssef.yu07@gmail.com>
2025-12-14 18:30:57 +0100

    Add script to list all tables in alx_book_store

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+-- This script lists all the tables in the database
+-- The database should be passed as an argument when running the MySQL command
+-- Example: mysql -u root -p alx_book_store < task_3.sql
+
+SHOW TABLES;
